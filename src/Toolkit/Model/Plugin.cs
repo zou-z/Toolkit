7c9fdf57a64dc5862320d4f30f333fc4544f1d96
zou-z <zzvr@outlook.com>
2022-11-20 20:21:05 +0800

    添加contract项目，鼠标点击相关命令绑定

--- src/Toolkit/Model/Plugin.cs
+++ src/Toolkit/Model/Plugin.cs
@@ -1,13 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+using NLog.Targets.Wrappers;
 
 namespace Toolkit.Model
 {
     internal class Plugin
     {
         public string Name { get; set; } = string.Empty;
+
+        public RelayCommand MouseLeftButtonDownCommand => mouseLeftButtonDownCommand ??= new RelayCommand(MouseLeftButtonDown);
+
+        public RelayCommand MouseLeftButtonUpCommand => mouseLeftButtonUpCommand ??= new RelayCommand(MouseLeftButtonUp);
+
+        public Plugin()
+        {
+        }
+
+        private void MouseLeftButtonDown()
+        {
+            Debug.WriteLine("触发了左键按下事件");
+            Mouse.Capture(Application.Current.MainWindow);
+
+        }
+
+        private void MouseLeftButtonUp()
+        {
+            Mouse.Capture(null);
+            Debug.WriteLine("触发了左键松开事件");
+        }
+
+        private RelayCommand? mouseLeftButtonDownCommand = null;
+        private RelayCommand? mouseLeftButtonUpCommand = null;
     }
 }
