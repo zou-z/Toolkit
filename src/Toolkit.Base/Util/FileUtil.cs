662a36c66c747666b0f26363ba30c1725aefa0f0
zou-z <zzvr@outlook.com>
2022-12-15 17:34:16 +0800

    添加设置读写的代码

---
+++ src/Toolkit.Base/Util/FileUtil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Toolkit.Base.Util
+{
+    public static class FileUtil
+    {
+        public static void SaveBytesToFile(string path, byte[] array)
+        {
+            using var fs = new FileStream(path, FileMode.Create);
+            fs.Write(array, 0, array.Length);
+        }
+
+        public static byte[] ReadBytesFromFile(string path)
+        {
+            if (!File.Exists(path))
+            {
+                return Array.Empty<byte>();
+            }
+            using var fs = new FileStream(path, FileMode.Open);
+            byte[] array = new byte[fs.Length];
+            fs.Read(array, 0, array.Length);
+            return array;
+        }
+    }
+}
---
+++ src/Toolkit.Base/Util/SettingUtil.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text.Json;
+using Toolkit.Base.Log;
+
+namespace Toolkit.Base.Util
+{
+    public class SettingUtil
+    {
+        public static void SaveSetting<T>(string name, T t) where T : class
+        {
+            try
+            {
+                string path = GetSettingPath(name, true);
+                var bytes = JsonSerializer.SerializeToUtf8Bytes(t, new JsonSerializerOptions
+                {
+                    WriteIndented = false
+                });
+                FileUtil.SaveBytesToFile(path, bytes);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, $"保存设置失败，设置名称：{name}", MethodBase.GetCurrentMethod()?.GetMethodName());
+            }
+        }
+
+        public static T? LoadSetting<T>(string name) where T : class
+        {
+            try
+            {
+                string path = GetSettingPath(name, false);
+                var bytes = FileUtil.ReadBytesFromFile(path);
+                var utf8Reader = new Utf8JsonReader(bytes);
+                return JsonSerializer.Deserialize<T>(ref utf8Reader);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, $"读取设置失败，设置名称：{name}", MethodBase.GetCurrentMethod()?.GetMethodName());
+            }
+            return null;
+        }
+
+        private static string GetSettingPath(string name, bool isSaveSetting)
+        {
+            if (path == string.Empty)
+            {
+                path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Settings");
+            }
+            if (isSaveSetting && !Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            return Path.Combine(path, name);
+        }
+
+        private static string path = string.Empty;
+    }
+}
