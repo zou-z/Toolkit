4be8554d4f22079fff1d50e005dc9ce6b47fcbb9
zou-z <zzvr@outlook.com>
2022-12-15 14:37:06 +0800

    修改多标签管理的代码位置

---
+++ src/JsonFormat/Model/TabManager.cs
@@ -0,0 +1,71 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using JsonFormat.View;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace JsonFormat.Model
+{
+    internal class TabManager : ObservableObject
+    {
+        public AsyncRelayCommand NewTabCommand => newTabCommand ??= new AsyncRelayCommand(NewTab);
+
+        public ObservableCollection<TabViewItem> Tabs => tabs;
+
+        public TabViewItem? SelectedItem
+        {
+            get => selectedItem;
+            set => SetProperty(ref selectedItem, value);
+        }
+
+        public TabManager()
+        {
+            tabs = new ObservableCollection<TabViewItem>();
+            for (int i = 0; i < 3; ++i)
+            {
+                tabs.Add(new TabViewItem
+                {
+                    Header = $"header {i + 1}",
+                    Content = CreateTabContent(),
+                });
+            }
+        }
+
+        private async Task NewTab()
+        {
+            await Application.Current.Dispatcher.BeginInvoke(() =>
+            {
+                var item = new TabViewItem
+                {
+                    Header = $"new header {++newTabIndex}",
+                    Content = CreateTabContent(),
+                };
+                if (SelectedItem != null)
+                {
+                    int index = Tabs.IndexOf(SelectedItem);
+                    Tabs.Insert(index + 1, item);
+                }
+                else
+                {
+                    Tabs.Add(item);
+                }
+                SelectedItem = item;
+            });
+        }
+
+        private static object CreateTabContent()
+        {
+            return new JsonFormatView();
+        }
+
+        private readonly ObservableCollection<TabViewItem> tabs;
+        private TabViewItem? selectedItem = null;
+        private AsyncRelayCommand? newTabCommand = null;
+        private int newTabIndex = 0;
+    }
+}
--- src/JsonFormat/ViewModel/MainViewModel.cs
+++ src/JsonFormat/ViewModel/MainViewModel.cs
@@ -21,54 +21,18 @@ namespace JsonFormat.ViewModel
 {
     internal class MainViewModel : ObservableObject
     {
-        public RelayCommand NewTabCommand => newTabCommand ??= new RelayCommand(NewTab);
+        public TabManager TabManager => tabManager ??= new TabManager();
 
         public AsyncRelayCommand FormatCommand => formatCommand ??= new AsyncRelayCommand(Format);
 
-        public ObservableCollection<TabViewItem> Tabs => tabs;
-
-        public TabViewItem? SelectedItem
-        {
-            get => selectedItem;
-            set => SetProperty(ref selectedItem, value);
-        }
-
         public MainViewModel()
         {
-            tabs = new ObservableCollection<TabViewItem>();
-            for (int i = 0; i < 3; ++i)
-            {
-                tabs.Add(new TabViewItem
-                {
-                    Header = $"header {i + 1}",
-                    Content = CreateTabContent(),
-                });
-            }
-        }
-
-        private void NewTab()
-        {
-            var item = new TabViewItem
-            {
-                Header = $"new header {++newTabIndex}",
-                Content = CreateTabContent(),
-            };
-            if (SelectedItem != null)
-            {
-                int index = Tabs.IndexOf(SelectedItem);
-                Tabs.Insert(index + 1, item);
-            }
-            else
-            {
-                Tabs.Add(item);
-            }
-            SelectedItem = item;
         }
 
         private async Task Format()
         {
-            if (SelectedItem == null) return;
-            if (SelectedItem.Content is IJsonFormatView jsonFormatView)
+            if (TabManager.SelectedItem == null) return;
+            if (TabManager.SelectedItem.Content is IJsonFormatView jsonFormatView)
             {
                 var jsonFormat = jsonFormatView.GetDataContext();
                 if (jsonFormat != null)
@@ -78,15 +42,7 @@ namespace JsonFormat.ViewModel
             }
         }
 
-        private static object CreateTabContent()
-        {
-            return new JsonFormatView();
-        }
-
-        private readonly ObservableCollection<TabViewItem> tabs;
-        private TabViewItem? selectedItem = null;
-        private RelayCommand? newTabCommand = null;
+        private TabManager? tabManager = null;
         private AsyncRelayCommand? formatCommand = null;
-        private int newTabIndex = 0;
     }
 }
